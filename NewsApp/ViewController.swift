7aa9ddfe0b20bce62196c925951132d9a0a285f7
Burak Donat <burak.donat@consultant.turkcell.com.tr>
2023-06-29 23:08:02 +0300

    Initial Commit

---
+++ NewsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by Burak Donat on 6/29/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
